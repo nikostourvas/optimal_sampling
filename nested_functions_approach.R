fcae38ff5a3f0b0aaef5944163a66c44c7c944ec
Nikolaos Tourvas <nikostourvas@gmail.com>
2019-02-18 23:14:52 +0200

    use of rapply function

--- nested_functions_approach.R
+++ nested_functions_approach.R
@@ -1,3 +1,7 @@
+
+# Simulations -------------------------------------------------------------
+
+
 # Original function
 sim_dataset_fun <- function(empirical){
   sim_data <- list()
@@ -12,7 +16,7 @@ sim_dataset_fun <- function(empirical){
 }
 
 # Nested function
-sims <- function(data){
+sim_fun <- function(data){
   res <- list()
   for(j in 1:length(data)){
     res[[j]] <- sim_dataset_fun(data[[j]])
@@ -21,4 +25,44 @@ sims <- function(data){
 }  
 
 
-x <- sims(data)
+sims <- sim_fun(data)
+
+
+# Hobs, Hexp --------------------------------------------------------------
+
+# Original function
+results_fun <- function(sim_dataset){
+  results <- list()
+  for(i in samp_size[-length(samp_size)]){
+    for(j in 1:replic_num){ # number of replications
+      results[[i]][[j]] <- summary(sim_dataset[[i]][[j]])
+    }
+  }
+  
+  results[[samp_size[length(samp_size)]]] <- 
+    summary(sim_dataset[[samp_size[length(samp_size)]]])
+  
+  return(results)
+}
+
+# Nested function
+res_fun <- function(sim_dataset){
+  res <- list()
+  for(j in 1:length(sim_dataset)){
+    res[[j]] <- results_fun(sim_dataset[[j]])
+  }
+  return(res)
+}
+
+res <- res_fun(sims)
+
+# rapply way
+# not faster, but way more elegant
+
+system.time({
+res_rap <- rapply(sims, summary, how = "replace")
+})
+
+system.time({
+  res <- res_fun(sims)
+})
